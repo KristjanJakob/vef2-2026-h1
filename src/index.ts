57ef52dcc79ddc73d99833011226d0b13e3898ab
Kristján Jakob Ásgrímsson <kristjanjakobasgrimsson@rfc1918.rhi.hi.is>
2026-03-05 10:22:47 +0000

    laga villur frá Render.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { createApp } from './app';
 
 const app = createApp();
 
-const port = Number(process.env.PORT ?? 3000);
+const port = Number(process.env.PORT) || 3000;
+const host = '0.0.0.0';
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running on http://${host}:${port}`);
 });
